5aaca9a01d78bc0386a25f6f52412f4979c3e6b7
Guilherm Santos <gui07015@gmail.com>
2019-05-18 10:23:41 -0300

    Add

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <a href=" ">Menu</a>
+    <input type="text"
+    placeholder="Search for Book"
+    value={}/>
+  </div>
+);
+
+export default Header;
